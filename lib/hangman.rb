66e0039e78e51ba3c8b940d0f2f89a67880acbc8
HSaad <hagrsaad16@gmail.com>
2018-03-08 21:06:56 -0500

    Started serialization method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,21 +1,21 @@
 class Game
+	@@id = 0
 
 	def initialize
 		@word_maker = Word.new
 		@board = Board.new()
 		@player = Player.new()
+		@@id += 1
 	end
 
 	def start
 		print_instructions()
-		#open saved game? or new game
+		new_or_save()
 		set_word()
 		@board.display_word(@player, @word)
 
 		while (!game_over?)
-			# ask to save game?
-			save?()
-			@player.guess_word()
+			save_or_guess()
 			@board.draw(@player, @word)
 		end
 
@@ -78,8 +78,7 @@ class Game
 		end
 	end
 
-	def restart
-		#ask if they would like to start new game or open saved game
+	def new_or_save
 		puts "Choose your game: "
 		puts "1: New Game"
 		puts "2: Saved Game"
@@ -89,15 +88,14 @@ class Game
 	def get_game()
 		response = gets.chomp
 		if (response.strip == "1" || response.downcase.strip == "new game")
-			new_game()
 		elsif (response.strip == "2" || response.downcase.strip == "saved game")
 			load_game()
-		else
-			restart()
+		elsif (response.strip != "1" || response.downcase.strip != "new game")
+			new_or_save()
 		end
 	end
 
-	def new_game()
+	def restart()
 		@word_maker = Word.new
 		@board = Board.new()
 		@player = Player.new()
@@ -105,16 +103,38 @@ class Game
 		start()
 	end
 
-	def save?
-		puts ""
+	def save_or_guess
+		puts "To save the current game, type 'save'."
+		puts "Your Guess: "
+		response = gets.chomp
+		if response.downcase.strip == "save"
+			save_game()
+		else
+			@player.guess_word(response)
+		end
 	end
 
+	#use serialization to save game state
 	def save_game
 		Dir.mkdir("saved") unless Dir.exists? "saved"
-		filename = "saved/" #save the game object as json/xml
+		filename = "saved/game_#{@@id}.json" #save the game object as json/xml with id
+		
+		File.open(filename, 'w') do |file|
+			file.puts #game object serialization
+		end
 	end
 
-	def load_game
+	#method to pick saved game file to open (asks user for filename)
+	def choose_saved_game()
+		puts "Pick a saved game to load: "
+		#print out entries in the directory
+
+		filename = gets.chomp
+		#load_game(file) if file_valid
+	end
+
+	#use serialization to open up saved game
+	def load_game(filename)
 
 	end
 
@@ -126,11 +146,11 @@ class Game
 
 		def pick_word
 			word = File.readlines("5desk.txt").sample 
-			@code = word.strip if valid_word?(word) 
+			@code = word.strip.downcase if valid_word?(word) 
 		end
 
 		def valid_word?(word)
-			if word.length > 4 && word.length < 13 #word valid
+			if word.length > 5 && word.length < 13 #word valid
 				return true
 			else
 				pick_word
@@ -201,9 +221,7 @@ class Game
 			@guesses = []
 		end
 
-		def guess_word()
-			puts "\nYour Guess: "
-			guess = gets.chomp
+		def guess_word(guess)
 			if valid_guess?(guess)
 				@guesses << guess.downcase.strip
 			end
@@ -212,13 +230,15 @@ class Game
 		def valid_guess?(guess)
 			if @guesses.include?(guess.downcase.strip)
 				puts "You've already guessed '#{guess}'. Please try another letter."
-				guess_word
+				response = gets.chomp
+				guess_word(response)
 				return false
 			elsif guess.strip.length == 1 && letter?(guess.strip) == 0 
 				return true
 			else 
 				puts "Invalid Guess. Please try again."
-				guess_word
+				response = gets.chomp
+				guess_word(response)
 				return false
 			end
 		end
